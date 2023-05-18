1c0bf60c9563e0eeed9e34981b7b50465eb94058
gakuto45 <gakuto0122@gmail.com>
2023-05-18 20:56:20 +0900

    DFD0を修正，関数の枠だけ作った

--- 人類ギャルゲー化計画/Development/HumanGalgeeSystem/main.py
+++ 人類ギャルゲー化計画/Development/HumanGalgeeSystem/main.py
@@ -10,3 +10,28 @@ import mediapipe as mp
 from utils import CvFpsCalc
 
 
+def main():
+    return
+
+def culculate_face_pos_and_size(video):
+    
+    return posX,posY,posZ,sizeW,sizeH
+
+def resize_illustsize(image,posZ):
+
+    return resize_image
+
+def trim_face(posX,posY,posZ,sizeW,sizeH,image):
+    
+    return faceimage
+
+def conv_face2girl(faceimage):
+
+    return girlimage
+
+
+
+
+if __name__ == '__main__':
+    main()
+
