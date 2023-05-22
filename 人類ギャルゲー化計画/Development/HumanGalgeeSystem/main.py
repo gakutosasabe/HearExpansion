8f91174becc3f07c2261f861d8f59d89124b3a69
gakuto45 <gakuto0122@gmail.com>
2023-05-22 20:16:31 +0900

    プログラム実行時の引数の定義と，カメラの準備のとこまで書いた

--- 人類ギャルゲー化計画/Development/HumanGalgeeSystem/main.py
+++ 人類ギャルゲー化計画/Development/HumanGalgeeSystem/main.py
@@ -9,8 +9,63 @@ import mediapipe as mp
 
 from utils import CvFpsCalc
 
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--device", type=int, default=0)
+    parser.add_argument("--model_complexity",
+                        help='model_complexity(0,1(default))',
+                        type=int,
+                        default=1)
+
+    parser.add_argument("--max_num_hands", type=int, default=2)
+    parser.add_argument("--min_detection_confidence",
+                        help='min_detection_confidence',
+                        type=float,
+                        default=0.7)
+    parser.add_argument("--min_tracking_confidence",
+                        help='min_tracking_confidence',
+                        type=int,
+                        default=0.5)
+
+    parser.add_argument('--use_brect', action='store_true')
+    parser.add_argument('--plot_world_landmark', action='store_true')
+
+    args = parser.parse_args()
+
+    return args
+
 
 def main():
+    # 引数解析 ###############################################################
+    args = get_args()
+
+    cap_device = args.device
+    cap_width = 960
+    cap_height = 540
+
+    model_complexity = args.model_complexity
+
+    max_num_hands = args.max_num_hands
+    min_detection_confidence = args.min_detection_confidence
+    min_tracking_confidence = args.min_tracking_confidence
+
+    use_brect = args.use_brect
+    plot_world_landmark = args.plot_world_landmark
+
+
+
+    # カメラ準備　###############################################################
+    cap = cv.VideoCapture(cap_device)
+    cap.set(cv.CAP_PROP_FRAME_WIDTH, cap_width)
+    cap.set(cv.CAP_PROP_FRAME_HEIGHT, cap_height)
+
+
+    posX,posY,posZ,sizeW,sizeH = culculate_face_pos_and_size(video)
+    faceimage = trim_face(posX,posY,posZ,sizeW,sizeH,video)
+    girlimage = conv_face2girl(faceimage)
+
+    
     return
 
 def culculate_face_pos_and_size(video):
@@ -21,7 +76,7 @@ def resize_illustsize(image,posZ):
 
     return resize_image
 
-def trim_face(posX,posY,posZ,sizeW,sizeH,image):
+def trim_face(posX,posY,posZ,sizeW,sizeH,video):
     
     return faceimage
 
